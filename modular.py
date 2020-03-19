b897883af2222bb4eba45ed3161967c05f04410c
Jacob Malimban <jacobcmalimban@gmail.com>
2020-03-19 14:23:59 -0400

    Modularize tiles

---
+++ modular.py
@@ -0,0 +1,69 @@
+from enum import Enum
+
+class TileType(Enum):
+    '''
+        each is four
+        ball, bamboo, char --> 1-9
+
+        flores: 8*4
+        
+        winds
+            east
+            south
+            west
+            north
+
+        dragons
+            red
+            green
+            white
+
+        unique
+            flower
+            season
+
+        blank tiles
+    '''
+
+    __order__ = 'BALL STICK CHARACTER FLORES'
+    
+    BALL = "Ball"
+    STICK = "Stick"
+    CHARACTER = "Character"
+
+    FLORES = "Flores" #'winds', 'dragons', 'blanks']
+
+
+class FloresFace(Enum):
+    __order__ = 'EAST SOUTH WEST NORTH RED GREEN WHITE FLOWER SEASON BLANK'
+
+    EAST = "East"
+    SOUTH = "South"
+    WEST = "West"
+    NORTH = "North"
+
+    RED = "Red"
+    GREEN = "Green"
+    WHITE = "White"
+
+    FLOWER = "Flower"
+    SEASON = "Season"
+
+    BLANK = "Blank"
+
+class Tile:
+
+    def __init__(self, value, face):
+        self.value = value
+        self.face = face
+
+    def __str__(self):
+        return str(self.value) + "\t" + str(self.face)
+
+    def __repr__(self):
+        '''
+        return str(self)
+        '''
+        return "\t" + str(self.value) + " " + str(self.face)
+        #'''
+    
